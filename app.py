cc6bc84fd8949d11653e200c843412dabadda54f
Osvaldo Trujillo <osvaldo.trujillo.ingenieria@gmail.com>
2023-05-02 00:08:42 -0600

    [ADD] fronted templates

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 
+from flask import render_template
+
 from config import DevelopConfig
 
 from services import create_app
@@ -10,7 +12,8 @@ TITLE = "Test Globant >"
 
 @app.route('/')
 def index():
-    return TITLE
+    return render_template('index.html',
+                           title=TITLE), 200
 
 if __name__ == '__main__':
     app.run()
